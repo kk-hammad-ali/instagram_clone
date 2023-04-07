68f47ff30f201043ac22aca515465f383cc58127
Hammad Ali <hammadali9064@gmail.com>
2023-04-07 22:21:30 +0500

    Four new packages installed firebase_auth firebase_core firebase_storage cloud_firestore App is connected to the firebase and 3 new app created (Andriod, iOS, Web) at Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:instagram_clone/firebase_options.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/utils/responsive/responsive_layout_controller.dart';
 import 'package:instagram_clone/utils/routes.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyC92ApbCY_YAHuNqGOJiplM7e0NzMtwq0k',
+        appId: '1:667154830899:web:41bd50e7e29e68be6b3546',
+        messagingSenderId: '667154830899',
+        projectId: 'instagram-clone-94bcf',
+        storageBucket: "instagram-clone-94bcf.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
     runApp(const MyApp());
